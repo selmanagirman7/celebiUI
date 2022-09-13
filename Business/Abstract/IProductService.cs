afea3682d0f885ed5a37fd4785ce3ac64abb5f03
selman <88801652+selmanagirman7@users.noreply.github.com>
2022-09-13 10:00:37 +0300

    Add files via upload

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        Product Get(int id);
+
+        void Add(Product product);
+        void Update(Product product);   
+        void Delete(Product product);
+        List<Product> GetAll();
+    }
+}
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public object GetProductById(int id)
+        {
+           return _productDal.GetList(x=>x.ProductId==id);
+        }
+
+        public Product Get(int id)
+        {
+            return _productDal.GetById(id);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+
+        public static void TADD(object p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
